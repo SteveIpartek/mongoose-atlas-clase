edb577d8fb708223833bd31857e6b79ff5967cb2
SteveIpartek <estebansuarezramos5@gmail.com>
2025-05-20 21:20:15 +0200

    postman

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-// index.js
-
 const express = require('express');
 const app = express();
 const PORT = 8080;
 const { dbConnection } = require('./config/config');
-const routes = require('./routes');
+const usersRoutes = require('./users');
 
 app.use(express.json());
 
-app.use('/', routes);
+
+app.use('/users', usersRoutes);
 
 dbConnection();
 
